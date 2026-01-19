5a836b2d366ce18ad5e2617799b6ac8497d68745
You have one new message <gabriel_pirastru@hotmail.fr>
2026-01-19 22:33:25 +0700

    acr

---
+++ acr.tf
@@ -0,0 +1,34 @@
+# acr.tf
+
+module "acr" {
+  source = "./modules/acr"
+
+  workload            = local.workload
+  environment         = local.environment
+  location            = local.location
+  instance            = local.instance
+  resource_group_name = module.resource_group.rg_name
+  common_tags         = local.common_tags
+
+  sku           = "Basic"
+  admin_enabled = false
+}
+
+# Allow AKS to pull images from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = module.aks.kubelet_identity.object_id
+  role_definition_name             = "AcrPull"
+  scope                            = module.acr.id
+  skip_service_principal_aad_check = true
+}
+
+# Outputs
+output "acr_name" {
+  description = "ACR name"
+  value       = module.acr.name
+}
+
+output "acr_login_server" {
+  description = "ACR login server URL"
+  value       = module.acr.login_server
+}
